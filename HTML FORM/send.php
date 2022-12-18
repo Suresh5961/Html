d14ace61fb0733023e779bd84dca5c6cb36c692c
Suresh5961 <Sureshkumarece87@gmail.com>
2022-12-18 22:37:07 +0530

    db

--- HTML FORM/send.php
+++ HTML FORM/send.php
@@ -1,17 +1,33 @@
-<?php
+<?php  
 
-$n = $_POST['stname'];
-$c = $_POST['stclass']; 
-$con = mysqli_connect("localhost","root","","school");
-$sql = "INSERT INTO student details(Student_Name,Student_Class) values('$n','$c')";
-$r=mysqli_query($con,$sql);
-if($r)
-{
-    echo "ADDED";
-}
-else
-{
-    echo "NOT ADDED";
-}
 
-?>
+if (isset($_POST['name']) && isset($_POST['message'])) {
+	include 'db_conn.php';
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$name = validate($_POST['name']);
+	$message = validate($_POST['message']);
+
+	if (empty($message) || empty($name)) {
+		header("Location: index.html");
+	}else {
+
+		$sql = "INSERT INTO test(name, message) VALUES('$name', '$message')";
+		$res = mysqli_query($conn, $sql);
+
+		if ($res) {
+			echo "Your message was sent successfully!";
+		}else {
+			echo "Your message could not be sent!";
+		}
+	}
+
+}else {
+	header("Location: index.html");
+}
