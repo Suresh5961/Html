eb6396e892ded5982201ee76536473166f602310
Suresh5961 <Sureshkumarece87@gmail.com>
2023-06-18 18:06:33 +0530

    js

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+//single line commend in js
+
+/*Multi line commend
+*/
+//alert('haii')
+
+// let amount=0
+// amount=7.98
+// console.log("your bill amount is Rs."+amount)
+
+let a= prompt("enter how many tiket you want?")
+console.log("you should paid ",Number(a)*20)
